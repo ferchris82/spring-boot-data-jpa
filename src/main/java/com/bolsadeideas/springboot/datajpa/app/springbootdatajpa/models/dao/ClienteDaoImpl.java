4e4e0fe8b0a13cf5c3595df8bcdc796d39673d0a
ChrisferDev <ferchris82@hotmail.com>
2024-03-12 11:22:33 -0500

    Versión 1.0

---
+++ src/main/java/com/bolsadeideas/springboot/datajpa/app/springbootdatajpa/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,26 @@
+package com.bolsadeideas.springboot.datajpa.app.springbootdatajpa.models.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsadeideas.springboot.datajpa.app.springbootdatajpa.models.entity.Cliente;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Repository
+public class ClienteDaoImpl implements IClienteDao{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @SuppressWarnings("unchecked")
+    @Transactional(readOnly = true)
+    @Override
+    public List<Cliente> findAll() {
+        return em.createQuery("from Cliente").getResultList();
+    }
+    
+}
---
+++ src/main/java/com/bolsadeideas/springboot/datajpa/app/springbootdatajpa/models/dao/IClienteDao.java
@@ -0,0 +1,10 @@
+package com.bolsadeideas.springboot.datajpa.app.springbootdatajpa.models.dao;
+
+import java.util.List;
+
+import com.bolsadeideas.springboot.datajpa.app.springbootdatajpa.models.entity.Cliente;
+
+public interface IClienteDao {
+    
+    public List<Cliente> findAll();
+}
