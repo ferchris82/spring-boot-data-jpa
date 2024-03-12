8c7ae14134d2fed98b4777e1f1403618b9be14bf
ChrisferDev <ferchris82@hotmail.com>
2024-03-12 16:18:02 -0500

    Versión 1.2

--- src/main/java/com/bolsadeideas/springboot/datajpa/app/springbootdatajpa/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/bolsadeideas/springboot/datajpa/app/springbootdatajpa/models/dao/ClienteDaoImpl.java
@@ -24,9 +24,25 @@ public class ClienteDaoImpl implements IClienteDao{
     }
 
     @Override
+    @Transactional(readOnly = true)
+    public Cliente findOne(Long id) {
+        return em.find(Cliente.class, id);
+    }
+
+    @Override
     @Transactional
     public void save(Cliente cliente) {
-        em.persist(cliente);
+        if(cliente.getId() != null && cliente.getId() >0){
+            em.merge(cliente);
+        } else {
+            em.persist(cliente);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) { 
+        em.remove(findOne(id));
     }
     
 }
